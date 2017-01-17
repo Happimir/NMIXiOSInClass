86a286c5bab7b5ab017ad83127a68f59fbcf0c1c
Michael Kovalsky <michaelkovalsky@NMI14.local>
2017-01-17 13:38:08 -0500

    adding new folder

---
+++ segueDemo/segueDemo/segueDemo/SegueViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SegueViewController.swift
+//  segueDemo
+//
+//  Created by Michael Kovalsky on 1/12/17.
+//  Copyright © 2017 Emuel. All rights reserved.
+//
+
+import UIKit
+
+class SegueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ segueDemo/segueDemo/segueDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  segueDemo
+//
+//  Created by Michael Kovalsky on 1/12/17.
+//  Copyright © 2017 Emuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
