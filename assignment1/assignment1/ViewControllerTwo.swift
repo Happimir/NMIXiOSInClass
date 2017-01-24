b76375d832c06db6be1f29e7fdb23dd83c50561c
Michael Kovalsky <michaelkovalsky@NMI14.local>
2017-01-24 12:55:41 -0500

    pushing

--- assignment1/assignment1/ViewControllerTwo.swift
+++ assignment1/assignment1/ViewControllerTwo.swift
@@ -10,7 +10,74 @@ import UIKit
 
 class ViewControllerTwo: UIViewController {
 
+    var timer : Timer!;
+    var index : Int = 0;
+    
+    var picsChosen : [String] = [];
+//    var picsChosen : [String] {
+//        set(newData) {
+//            for index in 0..<newData.count {
+//                print("Index is: ", newData[index]);
+//            }
+//            self._picsChosen = newData;
+//        } get {
+//            return self._picsChosen;
+//        }
+//    }
+    
+
+    @IBOutlet weak var showImage : UIImageView!;
+    
+    @IBAction func previous() {
+        if(timer != nil) {
+            timer.invalidate();
+        }
+        
+        index -= 1;
+        show();
+        
+    }
+    
+    func autoShow() {
+        index += 1;
+        if(index > picsChosen.count - 1) {
+            index = 0;
+        }
+
+        let displayPic = UIImage( named : picsChosen[index]);
+        showImage.image = displayPic;
+
+    }
+    
+    func show() {
+        //let answer = (number % 2 == 0) ? "even" : "odd"
+        //index = (index > picsChosen.count) ? 0 : index;
+        if(index > picsChosen.count - 1) {
+            index = 0;
+        }
+        if(index < 0) {
+            index = picsChosen.count - 1;
+        }
+        
+        let displayPic = UIImage( named : picsChosen[index]);
+        showImage.image = displayPic;
+    }
+    
+    @IBAction func next() {
+        if(timer != nil) {
+            timer.invalidate();
+        }
+        
+        index += 1;
+        show();
+    }
+    
+    @IBAction func auto() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.autoShow), userInfo: nil, repeats: true);
+    }
+    
     override func viewDidLoad() {
+        showImage.image = UIImage(named :picsChosen[0]);
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
--- assignment1/assignment1/assignment1/ViewController.swift
+++ assignment1/assignment1/assignment1/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var sLS1 : [String] = ["beach.jpg", "butterfly.jpg", "bigboy.jpg", "tree.jpg"];
+    var sLS2 : [String] = ["dino1.jpg", "dino2.jpg", "dino3.jpg", "mamut.jpg"];
+    
+    var myClass : ViewControllerTwo! = ViewControllerTwo();
+    
+    //Note to self, look at that SO answer. Also note how
+    //the storyboard actually did the segues for you,
+    //thus as we had it originally, these buttons are 
+    //pointless.
+    @IBAction func unifiedLogic(button: UIButton) {
+        self.performSegue(withIdentifier: "slideShow", sender: button)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let passedPic = segue.destination as! ViewControllerTwo
+        
+        if let button = sender as? UIButton {
+            if button.tag == 1 {
+                passedPic.picsChosen = sLS1
+            } else if button.tag == 2 {
+                passedPic.picsChosen = sLS2
+            }
+        }
+    }
 }
+
+
 
